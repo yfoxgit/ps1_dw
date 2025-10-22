0a03fe6a2ca604b0c2bac95417d5a74e5d8d5c35
yfoxgit <yasminfox1998@gmail.com>
2025-10-22 20:06:36 +0100

    shortened set up file, decided on pandas

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 # Data Wrangling Optional Exericse
 
-# set up
-import polars as pl
+# Creation of Data
+import pandas as pd
 
 # Task 1: recreate dataframe
-data = {"household_id":[1,2,3,3,4,5,5,5],
+df = pd.DataFrame({"household_id":[1,2,3,3,4,5,5,5],
         "person":[1,1,1,2,1,1,2,3],
         "age":[20, 40, 35, 10, 18, 2,30,70],
         "income": [20000, 50000, 60000, 0, 5000, 0, 40000, 12000],
-        "female": [True, False, True, False, False, True, True, True]}
-df = pl.DataFrame(data)
-
-# Task 2: develop functions
-
-# Task 3:
+        "female": [True, False, True, False, False, True, True, True]})
